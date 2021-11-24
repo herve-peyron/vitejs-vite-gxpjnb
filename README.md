82cd98a0f9ebd8b50b2fc21fe9ec90066104a607
herve-peyron <herve.peyron@protonmail.com>
2021-11-24 12:14:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-gxpjnb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-gxpjnb)
---
+++ src/components/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Ceci est un menu</h1>
+  <!-- ul>
+      <li>Choix 1</li>
+      <li>Choix 2</li>
+    </ul -->
+</template>
+
+<style lang="less">
+h1 {
+  color: red;
+}
+</style>
