4d44170a1699bfb3fcb0c66a1b4635537e25d8e9
herve-peyron <herve.peyron@protonmail.com>
2021-11-30 11:35:34 +0100

    1 file modified, 5 deleted

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
-<script setup>
-import Navbar from './components/Navbar.vue';
-</script>
-
 <template>
-  <navbar></navbar>
+  <div class="d1"></div>
 </template>
+
+<style lang="less">
+div {
+  width: 90%;
+  height: 200px;
+  margin: 0 auto 20px auto;
+  border: 2px solid black;
+  box-sizing: border-box;
+}
+.d1 {
+  background-color: orange;
+  animation-name: couleur;
+  animation-duration: 4s;
+}
+@keyframes couleur {
+  from {
+    background-color: orange;
+  }
+  to {
+    background-color: blue;
+  }
+}
+</style>
